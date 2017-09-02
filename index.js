5fdeae4604a885433af489ba047ac4bf785d87f6
Dan Hedgecock <hedgecock.d@gmail.com>
2017-09-01 19:50:19 -0700

    Fix using a test env for linting formatting and console

--- index.js
+++ index.js
@@ -20,17 +20,22 @@ const config = {
   rules: {
     // Allow use of named functions before declared, they are hoisted and this makes
     // it possible to declare propTypes at top of component files
-    'no-use-before-define': [1, { functions: false }]
+    'no-use-before-define': ['error', { functions: false }],
+    // Allow console logs in development, and upgrade them to error in test
+    'no-console': 'off'
   }
 }
 
-// Lint for prettier only in TEST envs
+// Set of rules that are only checked in `test` environment
 // Watch https://github.com/prettier/eslint-plugin-prettier/issues/46 for .prettierrc
-if (process.env.NODE_ENV === 'TEST') {
-  config.rules['prettier/prettier'] = [
-    'error',
-    { singleQuote: true, printWidth: 84, semi: false }
-  ]
+if (process.env.NODE_ENV === 'test') {
+  Object.assign(config.rules, {
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, printWidth: 84, semi: false }
+    ],
+    'no-console': 'error'
+  })
 }
 
 module.exports = config
