23ab4f540cefa4a3d381a438d256f4beb5eb68a4
Dan Hedgecock <hedgecock.d@gmail.com>
2017-10-01 17:43:17 -0700

    Turn off sort-comp

--- web.js
+++ web.js
@@ -19,6 +19,9 @@ module.exports = {
   rules: {
     // Allow use of named functions before declared, they are hoisted and this makes
     // it possible to declare propTypes at top of component files
-    'no-use-before-define': ['error', { functions: false }]
+    'no-use-before-define': ['error', { functions: false }],
+    // Class ordering currently doesn't support class property syntax, which is 🙅
+    // Update on: https://github.com/yannickcr/eslint-plugin-react/pull/685
+    'react/sort-comp': 'off'
   }
 }
