a6f1865a6da3675138a35b686bec65419a642298
Dan Hedgecock <hedgecock.d@gmail.com>
2018-04-29 13:43:46 -0700

    chore: update commitizen template

--- .cz-config.js
+++ .cz-config.js
@@ -2,31 +2,32 @@
 
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:     A new feature' },
-    { value: 'fix', name: 'fix:      A bug fix' },
-    { value: 'docs', name: 'docs:     Documentation only changes' },
-    {
-      value: 'style',
-      name:
-        'style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-    },
+    { value: 'feat', name: '🎉 feat:       A new feature' },
+    { value: 'fix', name: '👍 fix:        A bug fix' },
     {
       value: 'refactor',
-      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+      name:
+        '🔩 refactor:   A code change that neither fixes a bug nor adds a feature',
     },
-    { value: 'perf', name: 'perf:     A code change that improves performance' },
-    { value: 'test', name: 'test:     Adding missing tests' },
+    {
+      value: 'perf',
+      name: '🏎  perf:       A code change that improves performance',
+    },
+    {
+      value: 'test',
+      name: '✅ test:       Updating tests or adding missing tests',
+    },
     {
       value: 'chore',
       name:
-        'chore:    Changes to the build process or auxiliary tools and libraries such as documentation generation',
+        '⬆️  chore:      Changes to the build process or development tools and libraries',
     },
-    { value: 'revert', name: 'revert:   Revert to a commit' },
-    { value: 'WIP', name: 'WIP:      Work in progress' },
+    { value: 'docs', name: '📝 docs:       Documentation only changes' },
+    { value: 'revert', name: '↩️  revert:     Revert to a commit' },
+    { value: 'WIP', name: '🚧 wip:        Work in progress' },
   ],
 
-  scopes: [{ name: 'base' }, { name: 'web' }, { name: 'node' }],
-
-  allowCustomScopes: true,
+  allowCustomScopes: false,
   allowBreakingChanges: ['feat', 'fix', 'style', 'refactor', 'perf', 'chore'],
+  scopes: [],
 }
