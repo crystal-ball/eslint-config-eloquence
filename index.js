dd402949c5e5de460851134930d9498aaeeba6c4
Dan Hedgecock <dan.hedgecock@nodesource.com>
2018-07-18 22:54:11 -0700

    feat(react): add react style warn, fix object spread change breaking VSCode
    VSCode is still using Node 7.9 😭

--- index.js
+++ index.js
@@ -37,25 +37,31 @@ module.exports = {
       : { node: { extensions: ['.js', '.mjs', '.json'] } }
   },
 
-  rules: {
-    // --- 🌬 Flow
-    // See: https://github.com/gajus/eslint-plugin-flowtype
-    'flowtype/define-flow-type': 'warn',
-    'flowtype/require-valid-file-annotation': 'warn',
-    'flowtype/use-flow-type': 'warn',
+  rules: Object.assign(
+    {
+      // --- 🌬 Flow
+      // See: https://github.com/gajus/eslint-plugin-flowtype
+      'flowtype/define-flow-type': 'warn',
+      'flowtype/require-valid-file-annotation': 'warn',
+      'flowtype/use-flow-type': 'warn',
 
-    // --- ⬆️ Updates/Enhancements
-    // Don't enforce .jsx file extension, it doesn't provide a clear benefit and
-    // often requires addl configs on other tooling, do less ¯\_(ツ)_/¯
-    'react/jsx-filename-extension': 'off',
-    // Include .mjs file extension in list of file that shouldn't use ext
-    'import/extensions': ['error', 'always', { js: 'never', jsx: 'never', mjs: 'never' }],
+      // --- ⬆️ Updates/Enhancements
+      // Don't enforce .jsx file extension, it doesn't provide a clear benefit and
+      // often requires addl configs on other tooling, do less ¯\_(ツ)_/¯
+      'react/jsx-filename-extension': 'off',
+      // Include .mjs file extension in list of file that shouldn't use ext
+      'import/extensions': [
+        'error',
+        'always',
+        { js: 'never', jsx: 'never', mjs: 'never' }
+      ],
 
-    // --- 🐛 Bugs
+      // --- 🐛 Bugs
 
-    // Class ordering currently doesn't support class property syntax, which is 🙅
-    // Update on: https://github.com/yannickcr/eslint-plugin-react/pull/685
-    'react/sort-comp': 'off',
+      // Class ordering currently doesn't support class property syntax, which is 🙅
+      // Update on: https://github.com/yannickcr/eslint-plugin-react/pull/685
+      'react/sort-comp': 'off'
+    },
 
     // 🌍 Environment adjustments
     // ---------------------------------------------------------------------------
@@ -67,7 +73,7 @@ module.exports = {
      * (especially when using webpack dev server hooked into the errors overlay
      * 😉)
      */
-    ...(NODE_ENV === 'test'
+    NODE_ENV === 'test'
       ? {
           // Validate formatting is correct in test,prod,etc.
           'prettier/prettier': 'error'
@@ -79,19 +85,25 @@ module.exports = {
 
           // These rules are non critical, stylistic rules, warn only in dev for them
           'arrow-body-style': 'warn',
+          'class-methods-use-this': 'warn',
           'no-unused-vars': 'warn',
           'prefer-const': 'warn',
           'prefer-destructuring': 'warn',
 
           'react/default-props-match-prop-types': 'warn',
+          'react/destructuring-assignment': 'warn',
           'react/forbid-prop-types': 'warn',
+          'react/jsx-boolean-value': 'warn',
+          'react/no-multi-comp': 'warn',
+          'react/no-unescaped-entities': 'warn',
           'react/no-unused-prop-types': 'warn',
           'react/no-unused-state': 'warn',
           'react/prefer-stateless-function': 'warn',
           'react/prop-types': 'warn',
           'react/require-default-props': 'warn',
+          'react/self-closing-comp': 'warn',
 
           'import/first': 'warn'
-        })
-  }
+        }
+  )
 }
