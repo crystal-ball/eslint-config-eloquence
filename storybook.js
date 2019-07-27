6c95b7432540dcd995d74720e12bba2cbeb35b00
Dan Hedgecock <hedgecock.d@gmail.com>
2019-07-27 06:16:40 -0700

    Fix: Allow importing path in Storybook configs 🔧
    Closes #88

--- storybook.js
+++ storybook.js
@@ -6,6 +6,14 @@
  */
 module.exports = {
   extends: ['./react.js'],
+
+  settings: {
+    // Storybook setup uses the webpack resolver (inside React apps), but also
+    // needs to import the core path module for configuring the Storybook
+    // specific webpack configs
+    'import/core-modules': ['path'],
+  },
+
   env: {
     node: true,
   },
