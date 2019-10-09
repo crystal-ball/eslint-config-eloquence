4e70da461311c8204157cbe5bcf55787848719ee
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-10-09 08:44:15 -0700

    New: Rules added in new ESLint and plugin versions 💖 (#130)
    Including: default params last, prefer literal regex construction, require data selectors for
    cypress, forbid useless fragments, and require default props are alphabetical
    
    Closes #129

--- src/rules/cypress.js
+++ src/rules/cypress.js
@@ -4,14 +4,18 @@
  * Rules for Cypress acceptance tests
  */
 module.exports = {
+  // Require an assertion before a screenshot to help ensure consistent screenshots
+  'cypress/assertion-before-screenshot': 'error',
+
   // Use closures to access the async values returned by Commands
   'cypress/no-assigning-return-values': 'error',
 
   // Use route aliases or assertions instead of `cy.wait` calls
   'cypress/no-unnecessary-waiting': 'error',
 
-  // Require an assertion before a screenshot to help ensure consistent screenshots
-  'cypress/assertion-before-screenshot': 'error',
+  // Require that Cypress interactions use data-selectors, eg data-testid, to
+  // help encourage test resiliency
+  'cypress/require-data-selectors': 'error',
 
   // --- ⬆️ Rule overrides ---
 
