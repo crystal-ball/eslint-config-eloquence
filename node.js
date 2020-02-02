3241d8d471671a535db494d4733c3b170d540eff
Dan Hedgecock <hedgecock.d@gmail.com>
2020-02-02 08:07:30 -0800

    New: Add file overrides to Node and React projects 💖
    File overrides enable setting configs for specific globs, removing the need for tool specific entry
    files. Eg extending eloquence/storybook is no longer required and is now automagically handled.
    
    BREAKING CHANGE: Storybook and Cypress entry files are removed
    
    RELEASE NOTES: Remove eloquence/storybook and eloquence/cypress config files, remove eslint comments
    for webpack.config.js

--- node.js
+++ node.js
@@ -1,21 +1,74 @@
 'use strict'
 
+const coreBestPractices = require('./src/core-best-practices')
+const coreEcmaScript = require('./src/core-ecma-script')
+const corePossibleErrors = require('./src/core-possible-errors')
+const coreStylisticIssues = require('./src/core-stylistic-issues')
+const coreVariables = require('./src/core-variables')
+const imports = require('./src/imports')
+const node = require('./src/node')
 const devRuleOverrides = require('./src/dev-rule-overrides')
 
 /**
  * Node.js services use the base ruleset extended with the Node ruleset.
  */
 module.exports = {
-  extends: [
-    // Project rules, settings, etc. definitions
-    './src/base-config.js',
-    './src/node-config.js',
+  extends: ['prettier'],
 
-    // Prettier override
-    'prettier',
-  ],
+  parser: 'babel-eslint',
+
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'script',
+  },
+
+  plugins: ['import', 'prettier'],
+
+  settings: {
+    // Increase import cache lifetime to 60s
+    'import/cache': 60,
+  },
+
+  env: {
+    es6: true,
+    node: true,
+  },
 
   rules: {
+    // --- ESLint core rules configuration ---
+    ...coreBestPractices,
+    ...coreEcmaScript,
+    ...corePossibleErrors,
+    ...coreStylisticIssues,
+    ...coreVariables,
+
+    // Node specific rules
+    ...node,
+
+    // --- Plugin rules ---
+
+    ...imports,
+
+    // Prettier formatting enforcement via Prettier *plugin*
+    // (this is different from the rule overrides set in the Prettier *config*)
+    'prettier/prettier': 'error',
+
+    // --- Env aware overrides ---
     ...devRuleOverrides('node'),
   },
+
+  // Report on unused eslint-disable comments
+  reportUnusedDisableDirectives: true,
+
+  // --- File overrides -----------------------------------
+
+  overrides: [
+    // ✅ Spec files - Adds Jest globals used by tests
+    {
+      files: ['*.spec.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 }
