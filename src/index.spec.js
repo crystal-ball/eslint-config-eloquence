7c30ff42210775cef9949903747c64298b2bb05b
Dan Hedgecock <Hedgecock.D@gmail.com>
2020-05-31 09:16:57 -0600

    New: Configs for ignorePatterns and rules 💖 (#213)

--- src/index.spec.js
+++ src/index.spec.js
@@ -4,6 +4,23 @@ jest.mock('path')
 
 const eloquence = require('./index')
 
+describe('Configs', () => {
+  test('When ignorePatterns is passed, then the returned configs include them', () => {
+    const configs = eloquence({ target: 'node', ignorePatterns: ['coverage'] })
+    expect(configs.ignorePatterns).toStrictEqual(['coverage'])
+  })
+
+  test('When rules is passed, then they have precedence', () => {
+    const configs = eloquence({
+      target: 'node',
+      rules: {
+        'no-var': 'off',
+      },
+    })
+    expect(configs.rules['no-var']).toBe('off')
+  })
+})
+
 describe('Node configs', () => {
   test('The configs match the snapshot', () => {
     expect(eloquence({ target: 'node' })).toMatchSnapshot()
