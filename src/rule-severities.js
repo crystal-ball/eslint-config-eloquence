b4b75eb0d270398aaab6c09aacde1820883d2912
Dan Hedgecock <hedgecock.d@gmail.com>
2020-05-30 20:28:27 -0700

    Fix: Disable import/no-unused-imports entirely ⚙️

--- src/rule-severities.js
+++ src/rule-severities.js
@@ -14,9 +14,7 @@ const severityOverrides = {
   'sort-imports': 'warn',
 
   // Imports style preferences rules
-  'import/first': 'warn',
   'import/order': 'warn',
-  'import/no-unused-modules': 'off',
 
   // Don't show any Prettier formatting errors during dev b/c they're SUPER
   // noisy and annoying and NOT helpful at that time
