c4f71afe0056398e734e2071005567c5764fb6c0
Dan Hedgecock <hedgecock.d@gmail.com>
2020-11-21 13:04:49 -0800

    Update: package dependencies 🆙

--- src/rules/plugin-react.js
+++ src/rules/plugin-react.js
@@ -409,22 +409,4 @@ module.exports = {
   // Enforce that Flow props types are read-only
   // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-read-only-props.md
   'react/prefer-read-only-props': 'off',
-
-  // --- Prettier managed rules ---------------------------
-  // https://github.com/prettier/eslint-config-prettier/blob/master/react.js
-
-  'react/jsx-child-element-spacing': 'off',
-  'react/jsx-closing-bracket-location': 'off',
-  'react/jsx-closing-tag-location': 'off',
-  'react/jsx-curly-newline': 'off',
-  'react/jsx-curly-spacing': 'off',
-  'react/jsx-equals-spacing': 'off',
-  'react/jsx-first-prop-new-line': 'off',
-  'react/jsx-indent': 'off',
-  'react/jsx-indent-props': 'off',
-  'react/jsx-max-props-per-line': 'off',
-  'react/jsx-one-expression-per-line': 'off',
-  'react/jsx-props-no-multi-spaces': 'off',
-  'react/jsx-tag-spacing': 'off',
-  'react/jsx-wrap-multilines': 'off',
 }
