59a6254fa3c478565f61f3af2d95757661795527
Dan Hedgecock <Hedgecock.D@gmail.com>
2020-11-27 14:20:45 -0800

    Update plugin-jest-dom.js

--- src/rules/plugin-jest-dom.js
+++ src/rules/plugin-jest-dom.js
@@ -12,6 +12,7 @@ module.exports = {
   'jest-dom/prefer-empty': 'error',
   'jest-dom/prefer-enabled-disabled': 'error',
   'jest-dom/prefer-focus': 'error',
+  'jest-dom/prefer-in-document': 'error',
   'jest-dom/prefer-required': 'error',
   'jest-dom/prefer-to-have-attribute': 'error',
   'jest-dom/prefer-to-have-style': 'error',
